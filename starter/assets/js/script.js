c6e5a91bf1e58d4d9c31a0cedaa4795b6a02b472
sdum72 <sdum@hotmail.com>
2023-11-30 08:04:32 +0000

    Practice exercises completed

---
+++ starter/assets/js/script.js
@@ -0,0 +1,8 @@
+// Access element using id
+var articlesDiv = document.getElementById('articles');
+var headerDiv = document.getElementById('header');
+
+// Change style by accessing style object's properties
+articlesDiv.children[0].style.fontSize = '10px';
+headerDiv.children[0].style.color = 'white';
+
